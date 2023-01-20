40abc4d8812e5deed2a72ebc86a631bc9cbc670b
Zoe Thexton <zoethexton.dev@gmail.com>
2023-01-20 18:09:15 +0100

    Add basic Timber theme

--- wp-content/themes/swytch/index.php
+++ wp-content/themes/swytch/index.php
@@ -1,5 +1,23 @@
 <?php
-// There is nothing output here because block themes do not use php templates.
-// There is a core ticket discussing removing this requirement for block themes:
-// https://core.trac.wordpress.org/ticket/54272.
+/**
+ * The main template file
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
 
+$context          = Timber::context();
+$context['posts'] = new Timber\PostQuery();
+$context['foo']   = 'bar';
+$templates        = array( 'index.twig' );
+if ( is_home() ) {
+	array_unshift( $templates, 'front-page.twig', 'home.twig' );
+}
+Timber::render( $templates, $context );
---
+++ wp-content/themes/swytch/popular-accessories.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Template Name: Popular accessories
+ * Description: A page template to show the most popular accessories.
+ */
+
+$context = Timber::context();
+Timber::render( 'popular-accessories.twig', $context );
