071230516e1536a71123f4d0b16fdb868d71ef0a
Zoe Thexton <zoethexton.dev@gmail.com>
2023-01-21 10:38:16 +0100

    Add ACF to composer file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./**/*.php', '!**/vendor/**', './**/*.twig'],
+	content: ['!**/vendor/**', './**/*.twig'],
 	theme: {
 		extend: {},
 	},
